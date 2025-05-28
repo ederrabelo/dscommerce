b8c04db99d483b4de3bcfaa7c7d58b391bb1bdff
Eder Rabelo <eafonsorabelo@gmail.com>
2025-05-27 21:01:09 -0400

    Initial commit

---
+++ src/test/java/com/ederrabelo/dscommerce/DscommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ederrabelo.dscommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
