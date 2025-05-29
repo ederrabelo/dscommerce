44422e210d3d3a7bd61a316d98ecddab74c2b739
Eder Rabelo <eafonsorabelo@gmail.com>
2025-05-28 22:41:42 -0400

    Payment

--- src/main/java/com/ederrabelo/dscommerce/entities/Order.java
+++ src/main/java/com/ederrabelo/dscommerce/entities/Order.java
@@ -13,6 +13,7 @@ public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
     private OrderStatus status;
@@ -21,16 +22,21 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
 
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
         this.client = client;
+        this.payment = payment;
     }
+
 
     public Long getId() {
         return id;
@@ -64,6 +70,14 @@ public class Order {
         this.client = client;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -83,6 +97,7 @@ public class Order {
                 ", moment=" + moment +
                 ", status=" + status +
                 ", client=" + client +
+                ", payment=" + payment +
                 '}';
     }
 }
