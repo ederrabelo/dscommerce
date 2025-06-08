eb2bc6f2ec50297aefc4442831a3b3dbe01d370f
Eder Rabelo <eafonsorabelo@gmail.com>
2025-06-07 22:13:14 -0400

    Equals and hashCode

--- src/main/java/com/ederrabelo/dscommerce/entities/OrderItem.java
+++ src/main/java/com/ederrabelo/dscommerce/entities/OrderItem.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem {
@@ -47,5 +49,18 @@ public class OrderItem {
 
     public Integer getQuantity() {
         return quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
