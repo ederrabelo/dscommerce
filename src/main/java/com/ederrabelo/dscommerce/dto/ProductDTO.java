28c456cbe95fb78d0a8e3008031d4b7fa7b26426
Eder Afonso Rabelo <eafonsorabelo@gmail.com>
2025-07-09 21:30:14 -0400

    Bean validation

--- src/main/java/com/ederrabelo/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/ederrabelo/dscommerce/dto/ProductDTO.java
@@ -2,16 +2,28 @@ package com.ederrabelo.dscommerce.dto;
 
 import com.ederrabelo.dscommerce.entities.Product;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
     public ProductDTO() {
-        
+
     }
 
     public ProductDTO(Long id, String name, String description, Double price, String imgUrl) {
@@ -50,5 +62,4 @@ public class ProductDTO {
         return imgUrl;
     }
 
-    
 }
